8720ac66c91c78fb5aaf0fe4cc0edac6cca32d52
Andriy Orehov <andriyorehov@gmail.com>
2017-03-27 17:05:56 +0300

    refactor and update  dependencies

--- models.py
+++ models.py
@@ -2,15 +2,18 @@ from mongoengine import *
 
 connect('tumblelog')
 
+
 class User(Document):
     email = StringField(required=True)
     first_name = StringField(max_length=50)
     last_name = StringField(max_length=50)
     password = StringField(max_length=200)
 
+
 class Comment(EmbeddedDocument):
     content = StringField()
     name = StringField(max_length=120)
+
 
 class Post(Document):
     title = StringField(max_length=120, required=True)
@@ -19,11 +22,14 @@ class Post(Document):
     comments = ListField(EmbeddedDocumentField(Comment))
     meta = {'allow_inheritance': True}
 
+
 class TextPost(Post):
     content = StringField()
 
+
 class ImagePost(Post):
     image_path = StringField()
+
 
 class LinkPost(Post):
     link_url = StringField()
--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
-import os
-import myapp
-import unittest
 import tempfile
+import unittest
+
+import myapp
+
 
 class MyappTestCase(unittest.TestCase):
-
     def setUp(self):
         myapp.app.config['DEBUG'] = tempfile.mkstemp()
         self.app = myapp.app.test_client()
@@ -15,6 +15,7 @@ class MyappTestCase(unittest.TestCase):
     def test_index(self):
         rv = self.app.get('/')
         assert '<h2>Posts</h2>' in rv.data
+
 
 if __name__ == '__main__':
     unittest.main()
